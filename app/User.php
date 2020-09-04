b6bcf836cddb60d4537b14bc2b563720e52a0777
teambena <59677628+teambena@users.noreply.github.com>
2020-09-04 12:51:00 +0700

    Add files via upload

--- app/User.php
+++ app/User.php
@@ -15,6 +15,9 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+    protected $primaryKey = 'user_id';
+
     protected $fillable = [
         'name','lastname', 'email', 'password',
     ];
