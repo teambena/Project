4520f2425cd5f55754aa7250ae7bb0e05da8076b
teambena <59677628+teambena@users.noreply.github.com>
2020-09-05 15:37:59 +0700

    Add files via upload

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,7 +1,8 @@
+<!-- View Overall Data -->
 <!DOCTYPE html>
 <html>
     <head>
-        <title>View Student Records</title>
+        <title>View User Records</title>
     </head>
     <body>
         <table border = "1">
@@ -9,12 +10,19 @@
                 <td>Id</td>
                 <td>First Name</td>
                 <td>Last Name</td>
+                <td>Edit</td>
+                <td>Delete</td>
+
             </tr>
             @foreach ($users as $user)
             <tr>
                 <td>{{ $user->user_id }}</td>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->lastname }}</td>
+                <td><a href = 'edit/{{ $user->user_id }}'>Edit</a></td>    
+                <td><a href = 'delete/{{ $user->user_id }}'>Delete</a></td>
+            
+                <td></td>
             </tr>
             @endforeach
         </table>
